b530ef38af71853999e99f29dcc3f5178bb50da7
speedjh <hywogur0327@naver.com>
2026-03-04 11:32:49 +0900

    chore: Render 재배포 트리거 - 플레이스 명소 수정

--- app.py
+++ app.py
@@ -1484,3 +1484,4 @@ with app.app_context():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
+# redeploy trigger: place spot fix
